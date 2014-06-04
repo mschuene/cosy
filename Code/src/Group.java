a2efc0f47fb3743a9490838b6e557cd1de445503
Yaoxin Zhao <yaoxin@localhost>
2014-06-04 11:09:31 +0200

    was hast du gemacht

---
+++ Code/src/Group.java
@@ -0,0 +1,4 @@
+
+public enum Group {
+	PROXIMITY, SHAPE, COLOR
+}
--- Code/src/Main.java
+++ Code/src/Main.java
@@ -6,14 +6,14 @@ import java.util.ArrayList;
 
 /**
  * Write a description of class Main here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class Main {
-	
+
 	private static boolean on = true;
-    
+
     /**
      * Main-Method
      */
@@ -21,41 +21,42 @@ public class Main {
         ImageViewer viewer = new ImageViewer();
         viewer.setLocationRelativeTo(null);
         Thinkpad.setViewer(viewer);
-        
+
         while(on) {
         	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         	String line = reader.readLine();
         	Shape[] shapes = findShapes(line.split(" "));
             Color[] colors = findColors(line.split(" "));
+            Group[] group = findGroup(line.split(" "));
 	        if(shapes.length == 2 && colors.length == 2) {
 	        	int re1 = ShortTimedMemory.remember(colors[0], shapes[0]);
 	        	int re2 = ShortTimedMemory.remember(colors[1], shapes[1]);
 	           	if(re1 > -1) {
 	        		int i = Thinkpad.focusView(Thinkpad.peripheralView(Vision.getInput(), colors[1]), shapes[1]);
 	        		ShortTimedMemory.memorize(colors[1], shapes[1], i);
-	        		
+
 	        		System.out.println("I know of " + re1 + " " + colors[0].toString().toLowerCase()
 	                        + " " + shapes[0].toString().toLowerCase() + "s and counted " + i + " "
 	                        + colors[1].toString().toLowerCase() + " " + shapes[1].toString().toLowerCase() + "s"
 	                        + " altogther " + (re1 + i) + " Objects.");
-	        		
+
 	        	} else if (re2 > -1) {
 	        		int i = Thinkpad.focusView(Thinkpad.peripheralView(Vision.getInput(), colors[0]), shapes[0]);
 	        		ShortTimedMemory.memorize(colors[0], shapes[0], i);
-	        		
+
 	        		System.out.println("I know of " + re2 + " " + colors[1].toString().toLowerCase()
 	                        + " " + shapes[1].toString().toLowerCase() + "s and counted " + i + " "
 	                        + colors[0].toString().toLowerCase() + " " + shapes[0].toString().toLowerCase() + "s"
 	                        + " altogther " + (re2 + i) + " Objects.");
-	        		
+
 	        	} else {
 	        		System.out.println("Number unknown.");
 	        	}
-	        	
+
 	        } else if (shapes.length == 1 && colors.length == 1) {
 	        	int i = Thinkpad.focusView(Thinkpad.peripheralView(Vision.getInput(), colors[0]), shapes[0]);
 	        	ShortTimedMemory.memorize(colors[0], shapes[0], i);
-	        	
+
 	        	System.out.println("I counted " + i + " " + colors[0].toString().toLowerCase()
 	                    + " " + shapes[0].toString().toLowerCase() + "s.");
 	        } else {
@@ -65,9 +66,9 @@ public class Main {
 	        }
         }
     }
-    
+
     /**
-     * 
+     *
      */
     public static Shape[] findShapes(String[] args) {
         ArrayList<Shape> shapes = new ArrayList<Shape>(2);
@@ -78,15 +79,15 @@ public class Main {
             	} else {
             		shapes.add(Shape.valueOf(next.toUpperCase()));
             	}
-                
+
             } catch(IllegalArgumentException e) {
             }
         }
         return shapes.toArray(new Shape[shapes.size()]);
     }
-    
+
     /**
-     * 
+     *
      */
     public static Color[] findColors(String[] args) {
     	ArrayList<Color> colors = new ArrayList<Color>(2);
@@ -101,5 +102,23 @@ public class Main {
             }
         }
         return colors.toArray(new Color[colors.size()]);
+    }
+    /**
+     *
+     */
+    public static Group[] findGroup(String[] args) {
+        ArrayList<Group> group = new ArrayList<Group>(2);
+        for(String next : args) {
+            try {
+            	if(next.charAt(next.length()-1) == 's') {
+            		group.add(Group.valueOf(next.substring(0, next.length()-1).toUpperCase()));
+            	} else {
+            		group.add(Group.valueOf(next.toUpperCase()));
+            	}
+
+            } catch(IllegalArgumentException e) {
+            }
+        }
+        return group.toArray(new Group[group.size()]);
     }
 }
